ae86aa0b27e6f81980609f2563d2d98408b63735
Vinojan <vinojanmano95@gmail.com>
2022-11-23 22:33:24 +0530

    Routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-import Home from './pages/Home';
-import Form from './pages/Form';
-import WeatherFetch from './pages/WeatherFetch';
+import React, {Component} from 'react';
+import {Switch, Route, Link} from 'react-router-dom';
+// import Home from './pages/Home';
 
+/*
+const App =() =>{
+  return  (
+    <>
+    <Home/>
+    </>
 
+  );
+}
 
+/*
 function App() {
   return (
     <div className="App">
@@ -13,4 +22,63 @@ function App() {
     </div>
   );
 }
+export default App;
+*/
+
+class App extends Component {
+  render() {
+    return (
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">React Routing</h1>
+          </header>
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route path="/items" component={Items}/>
+            <Route path="/category" component={Category}/>
+          </Switch>
+        </div>
+    );
+  }
+}
+
+export const Home = () => (
+    <div>
+      Home Component
+      <ul>
+        <li>
+          <Link to="/items">Items</Link>
+        </li>
+        <li>
+          <Link to="/category">Category</Link>
+        </li>
+      </ul>
+    </div>
+);
+
+export const Items = () => (
+    <div>
+      <h1>Items Component</h1>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+        <li>Item 4</li>
+      </ul>
+    </div>
+
+);
+
+export const Category = () => (
+    <div>
+      <h1>Category Component</h1>
+      <ul>
+        <li>Category 1</li>
+        <li>Category 2</li>
+        <li>Category 3</li>
+        <li>Category 4</li>
+      </ul>
+    </div>
+);
+
 export default App;
